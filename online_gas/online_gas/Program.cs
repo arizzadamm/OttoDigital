6454cd7d9c6f2b370e8c46bb8eba2ea289638150
arizzadamm <arizadamar@live.com>
2020-06-03 16:46:32 +0700

    Upload Console

---
+++ online_gas/online_gas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace online_gas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
